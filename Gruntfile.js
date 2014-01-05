d63f545c62138394ea1ff39eeb6d76c9da5d9b8f
Yuki Suzuki <s12bte@gmail.com>
2014-01-06 02:55:06 +0900

    grunt-contrib-connectとgrunt-contrib-concatを追加

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,60 @@ module.exports = function(grunt){
 
   // プラグイン読み込み
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
   // 設定
   grunt.initConfig({
     less: {
-      //開発版のとき
       development: {
         files: {
-          "./css/sample.css": "./less/default.less"
+          //css file : less file
+          "../css/sample.css": "../less/default.less"
         }
       },
-      //製品版のとき
       production: {
         options: {
           cleancss: true
         },
         files: {
-          "./css/sample.css": "./less/default.less"
+          //css file : less file
+          "../css/sample.css": "../less/default.less"
+
         }
       }
     },
+
+    concat: {
+      development: {
+        //連結するファイル
+        src: ["../css/sample.css","../css/sample2.css"],
+        //出力ファイル
+        dest: "../css/sample.min.css"
+      },
+
     watch: {
+      options: {
+        livereload: true,
+      },
       //監視対象とするファイル
-      files: './less/default.less',
-      //変更があったらlessコンパイルの開発版を実行する
+      files: "../less/default.less",
+      //変更があったときのタスク
       tasks: ['less:development']
     },
 
+    connect: {
+      development: {
+        options: {
+          base: "../"
+        }
+      }
+    }
+
   });
 
-  //引数をなにもつけないでgruntを実行したときは、watchを実行する
-  grunt.registerTask('default', ["watch"]);
+  //タスク実行
+  grunt.registerTask('default', ["connect:development","watch"]);
 
 }
